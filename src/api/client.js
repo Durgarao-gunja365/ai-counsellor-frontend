d50fa492b498c0a3efcfc33ce5d534aa3ff1cad7
Durgarao <dgunja390@gmail.com>
2026-01-31 14:20:42 +0530

    inital commit2

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ai-counsellor-backend-2.onrender.com",
+  baseURL: "https://ai-counsellor-backend-3.onrender.com",
    withCredentials: true,
 });
 
